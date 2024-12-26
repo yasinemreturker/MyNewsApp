49a1bcd9f9beb0a30f352038e85430aa4c319d9a
EMRE <mr_turker@hotmail.com>
2024-12-26 17:19:33 +0300

    NA-02: Add splash screen structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Splash Api
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
 }
--- app/src/main/java/com/example/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/example/mynewsapp/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mynewsapp.ui.theme.MyNewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             MyNewsAppTheme {
